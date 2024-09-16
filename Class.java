c604422329177568604a3193744f5700c5f412b0
ljsama <2817982520@qq.com>
2024-09-16 21:36:08 +0800

    second commit

---
+++ Class.java
@@ -0,0 +1,39 @@
+package grammar;
+
+import java.util.Scanner;
+
+public class Class {
+    static class Cat {
+        String name;
+        int age;
+        String skin;
+        int weight;
+    }
+    public static void main(String[] args){
+        //定义函数并赋值
+        Cat cat1 = new Cat();
+        cat1.name = "小白";
+        cat1.age = 3;
+        cat1.skin = "白色";
+        cat1.weight = 10;
+        Cat cat2 = new Cat();
+        cat2.name = "小花";
+        cat2.age = 100;
+        cat2.skin = "花色";
+        cat2.weight = 20;
+        //问用户查看猫的信息
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("想要查看的猫(小白/小花):");
+        String name = myScanner.next();
+        //判断并输出
+        if (name.equals("小白")){
+            System.out.println("名字:" + cat1.name + " " + "年龄:" + cat1.age
+                                + "岁" + " " + "颜色:" + cat1.skin + " " + "体重:" + cat1.weight + "kg");
+        } else if (name.equals("小花")) {
+            System.out.println("名字:" + cat2.name + " " + "年龄:" + cat2.age
+                    + "岁" +" " + "颜色:" + cat2.skin + " " +"体重:" + cat2.weight + "kg");
+        } else {
+            System.out.println("您输入的名称未录入");
+        }
+    }
+}
