cddbfa9a5caef60abccf6bd7e1cdb1bd5ec418a1
ljsama <2817982520@qq.com>
2024-09-21 10:48:16 +0800

    third commit

--- Method.java
+++ Method.java
@@ -1,37 +1,37 @@
 package grammar;
 
 //创建方法
-class Person{
+class Person {
     String name;
     int age;
 
-    public void speak (){
+    public void speak () {
         System.out.println("我是一个好人");
     }
-    public void cal01(){
+    public void cal01() {
         int sum = 0;
-        for (int i = 0; i <= 1000; i++){
+        for (int i = 0; i <= 1000; i++) {
             sum += i;
         }
         System.out.println("1+..+1000的和=" + sum);
     }
     //形参
-    public void cal02(int n){
+    public void cal02(int n) {
         int sum = 0;
-        for (int i = 0; i <= n; i++){
+        for (int i = 0; i <= n; i++) {
             sum += i;
         }
         System.out.println("1+..+" + n + "的和=" + sum);
     }
     //两个形参
-    public int getSum(int num1, int num2){
+    public int getSum(int num1, int num2) {
         int res = num1 + num2;
         return res;
     }
 }
 
 public class Method {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Person p1 = new Person();
         p1.speak(); //调用方法
         p1.cal01();
