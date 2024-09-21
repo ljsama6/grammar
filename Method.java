274a6554e585a444fa369bcf32568a3386044a23
ljsama <2817982520@qq.com>
2024-09-21 10:18:52 +0800

    second commit

---
+++ Method.java
@@ -0,0 +1,15 @@
+package grammar;
+
+class Person {
+    String name;
+    int age;
+    public void speak () {
+        System.out.println("我是一个好人");
+    }
+}
+public class Method {
+    public static void main(String[] args){
+        Person p1 = new Person();
+        p1.speak();
+    }
+}
